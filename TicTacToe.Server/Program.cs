2ccb521930f25a27e3c6c171a4baf69c8a8411e5
Arinana <allyut12zk@gmail.com>
2026-02-12 15:22:57 +0300

    Изменение адреса фронта

--- TicTacToe.Server/Program.cs
+++ TicTacToe.Server/Program.cs
@@ -1,33 +1,36 @@
 using TicTacToe.Server.Hubs;
 using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews(); 
+builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddSignalR();
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("https://localhost:7255", "http://localhost:5286")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
-
+        policy.WithOrigins(
+            "https://localhost:7255",   
+            "http://localhost:5286",
+            "https://remarkable-fairy-da7f88.netlify.app"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
     });
 });
 
 var app = builder.Build();
 
+
 app.UseHttpsRedirection();
-app.UseDefaultFiles();
-app.UseStaticFiles();
 app.UseCors();
 
 app.UseRouting();
 app.MapControllers();
 app.MapRazorPages();
+
 app.MapHub<GameHub>("/gamehub");
-app.MapFallbackToFile("index.html");
 
 app.Run();
